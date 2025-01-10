7bd52e2fbd0c33737f811b72972ff44ff2f2e18f
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-10 18:16:35 +0700

    membuat data class FormError State

--- app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
@@ -71,3 +71,12 @@ data class InsertUiState(
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isEntryValid: FormErrorState = FormErrorState(),
 )
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+)
