a3bd71024c18e2733f4e76515094f827b8d6a625
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-10 17:20:00 +0700

    membuat fungsi trysend

--- app/src/main/java/com/example/pertemuan13/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan13/repository/RepositoryMhs.kt
@@ -3,7 +3,9 @@ package com.example.pertemuan13.repository
 import com.example.pertemuan13.model.Mahasiswa
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 
 interface RepositoryMhs {
@@ -26,5 +28,19 @@ class NetworkRepositoryMhs(
                     throw Exception("Gagal menambahkan data mahasiswa: " +
                             "${e.message}")
             }
+        }
+
+        override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
+                val mhsCollection = firestore.collection("Mahasiswa")
+                        .orderBy("nim", Query.Direction.DESCENDING)
+                        .addSnapshotListener{value, error ->
+
+                                if (value != null){
+                                        val mhsList = value.documents.mapNotNull {
+                                                it.toObject(Mahasiswa::class.java)!!
+                                        }
+                                        trySend(mhsList)// try send memberikan fungsi untuk mengim data ke flow
+                                }
+                        }
         }
 }
