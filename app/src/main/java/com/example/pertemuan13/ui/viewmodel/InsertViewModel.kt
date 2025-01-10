2ad7b8f117ac5c6f8fa3553ff6cbf31140aa2a99
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-10 17:43:26 +0700

    membuat class InsertViewModel

---
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan13.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan13.repository.RepositoryMhs
+
+class InsertViewModel(
+    private val mhs: RepositoryMhs
+): ViewModel(){
+
+}
