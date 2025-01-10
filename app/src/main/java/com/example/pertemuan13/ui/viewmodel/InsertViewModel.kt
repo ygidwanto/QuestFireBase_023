5f1f95ee9652457b5e97dbccfa90b3d269e68969
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-10 17:51:15 +0700

    membuat view model

--- app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
@@ -1,10 +1,22 @@
 package com.example.pertemuan13.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.pertemuan13.model.Mahasiswa
 import com.example.pertemuan13.repository.RepositoryMhs
+import com.google.firebase.events.Event
 
 class InsertViewModel(
     private val mhs: RepositoryMhs
 ): ViewModel(){
-
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent){
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
 }
