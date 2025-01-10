d81efa7464ddc9e4ca4348bbaafde96a8de1d090
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-10 21:15:43 +0700

    membuat DestinasiNavigasi

---
+++ app/src/main/java/com/example/pertemuan13/navigation/DestinasiNavigasi.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan13.navigation
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+object DestinasiHome: DestinasiNavigasi{
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
