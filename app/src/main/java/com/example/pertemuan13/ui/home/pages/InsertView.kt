499657813c80356dbba061b6ed45b344fbf521db
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-10 19:29:08 +0700

    membuat insert body mhs

--- app/src/main/java/com/example/pertemuan13/ui/home/pages/InsertView.kt
+++ app/src/main/java/com/example/pertemuan13/ui/home/pages/InsertView.kt
@@ -142,3 +142,42 @@ fun InsertBodyMhs(
         }
     }
 }
+
+@Composable
+fun InsertBodyMhs(
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent) -> Unit,
+    uiState: InsertUiState,
+    onClick: () -> Unit,
+    homeUiState: FormState
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        FormMahasiswa(
+            mahasiswaEvent = uiState.insertUiEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = homeUiState !is FormState.Loading,
+        ){
+            if (homeUiState is FormState.Loading){
+                CircularProgressIndicator(
+                    color = Color.White,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .padding(end = 8.dp)
+                )
+                Text("Loading")
+            }else{
+                Text("Add")
+            }
+        }
+    }
+}
